659eddeea3bde8a30abb4f5a2a95aa78afa02e34
MTK <MTK@MTK.com>
2017-02-13 09:38:32 -0500

    Initial commit of rtc6873w42

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rtc6873w42.mk
---
+++ BoardConfig.mk
@@ -0,0 +1,22 @@
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
+
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/rca/rtc6873w42/kernel
+
+BOARD_MKBOOTIMG_ARGS := --base 025f8000 --pagesize 4096 --kernel_offset 00008000 --ramdisk_offset fdf08000 --tags_offset fda08100 --second ~/omni/device/rca/rtc6873w42/recovery.img-second --second_offset ffe08010 --cmdline 'idle=halt notsc androidboot.hardware=sofiaboard apic=sofia nolapic_pm firmware_class.path=/system/vendor/firmware androidboot.selinux=permissive nolapic_timer x86_intel_xgold_timer=soctimer_only vmalloc=512m slub_max_order=0 console=ttyFIQ0,115200n8'  
+
+#MTK common twrp flags
+TW_NO_EXFAT := true
+TWHAVE_SELINUX := true
+#TW_USE_TOOLBOX := true
+TW_NO_EXFAT_FUSE := true
+TW_THEME := portrait_mdpi
+#RECOVERY_SDCARD_ON_DATA := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
+BOARD_CUSTOM_BOOTIMG_MK := device/rca/rtc6873w42/boot.mk
