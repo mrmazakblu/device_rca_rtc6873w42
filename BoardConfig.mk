8b66b8f92885e274eb7004601f419a9bbd97c9e5
mrmazakblu <mrmazakblu@gmail.com>
2017-02-13 18:46:41 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ TARGET_PREBUILT_KERNEL := device/rca/rtc6873w42/kernel
 
 BOARD_MKBOOTIMG_ARGS := --base 025f8000 --pagesize 4096 --kernel_offset 00008000 --ramdisk_offset fdf08000 --tags_offset fda08100 --second ~/omni/device/rca/rtc6873w42/recovery.img-second --second_offset ffe08010 --cmdline 'idle=halt notsc androidboot.hardware=sofiaboard apic=sofia nolapic_pm firmware_class.path=/system/vendor/firmware androidboot.selinux=permissive nolapic_timer x86_intel_xgold_timer=soctimer_only vmalloc=512m slub_max_order=0 console=ttyFIQ0,115200n8'  
 
-#MTK common twrp flags
+#Common twrp flags
 TW_NO_EXFAT := true
 TWHAVE_SELINUX := true
 #TW_USE_TOOLBOX := true
